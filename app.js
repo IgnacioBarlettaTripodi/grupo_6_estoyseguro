01456822f3aac5f1462406a1d9769de1d2a5c26b
manugordon <manuelgordon25@gmail.com>
2022-11-03 17:12:11 -0300

    Estilos de la pagina

--- app.js
+++ app.js
@@ -12,6 +12,19 @@ app.use(express.static("public"));
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname,"./views/home.html"))
 })
+
 app.get('/header', (req, res) => {
     res.sendFile(path.resolve(__dirname,"./views/header.html"))
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname,"./views/register.html"))
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname,"./views/login.html"))
+})
+
+app.get('/footer', (req, res) => {
+    res.sendFile(path.resolve(__dirname,"./views/footer.html"))
 })
