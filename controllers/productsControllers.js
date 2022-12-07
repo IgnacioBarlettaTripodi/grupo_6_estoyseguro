f140505e8ac3d7c2c1331597ba6e892f81aac7a4
manugordon <manuelgordon25@gmail.com>
2022-12-07 11:21:24 -0300

    Todas las rutas ya tienen su funcionalidad

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -18,10 +18,58 @@ let productsControllers = {
     create: (req,res)=>{
         res.render('product-create-form')
     },
+    store: (req,res)=>{
+
+        let newProduct = {
+            'id': products[products.length-1].id +1,
+            'name': req.body.name,
+            'price': req.body.price,
+            'duration': req.body.duration,
+            'category': req.body.category,
+            'description': req.body.description,
+        };
+        products.push(newProduct);
+        //Ahora tenemos que volver a convertirlo en JSON para sobreescbibir el JSON
+
+        fs.writeFileSync(productsFilePath, JSON.stringify(products,null,'\t'))
+
+        res.redirect('/')
+    },
     edit: (req, res) => {
 		let product = products.find(product => product.id == req.params.id)
 		res.render('product-edit-form', {product})
+    },
+    update:(req,res) => {
+        let productToEdit = products.find(product => product.id == req.params.id)
+        let productEdited = {
+			'id': productToEdit.id,
+			'name': req.body.name,
+			'price': req.body.price,
+			'duration': req.body.duration,
+			'category': req.body.category,
+			'description': req.body.description
+		};
+        let newProducts = products.map(product => {
+			if(productToEdit.id == product.id){ 
+				return product = productEdited
+            }else{
+                return product
+            }
+		})
+        fs.writeFileSync(productsFilePath, JSON.stringify(newProducts,null,'\t'))
+
+        res.render('product-detail-form', {product:productEdited})
+
+    },
+    delete: (req,res)=>{
+        let id = req.params.id
+        let newProducts = products.filter(product => product.id != id)
+
+        fs.writeFileSync(productsFilePath, JSON.stringify(newProducts,null,'\t'))
+        res.render('products',{products: newProducts})
+
     }
+
 }
 
 module.exports = productsControllers
